5ac4f7c15fcac47f463b0963b3a2af22cf859a56
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-21 16:49:07 -0500

    updated

--- PostgreSQL_basic_SELECT_statement.sql
+++ PostgreSQL_basic_SELECT_statement.sql
@@ -13,6 +13,13 @@ ORDER BY first_name DESC;
 
 --- Write a query to get the names (first_name, last_name), salary and 15% of salary as PF for all the employees.
     --- create new PF column as 15% of their salary (aggregate in select statement)
-    --- filter to only first_name, last_name, and salary columnn
+    --- select columns: first_name, last_name, and salary columnn
 SELECT first_name, last_name, salary, salary*.15 PF
 FROM employees;
+
+-- Write a query to get the employee ID, names (first_name, last_name) and salary in ascending order according to their salary.
+    --- select columns: employee_id, first_name, last_name, and salary
+    ---- order by salary
+SELECT employee_id, first_name, last_name, salary
+FROM employees
+ORDER BY salary ASC;
