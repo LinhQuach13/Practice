e7f22b6f42d2daf682bede9e9980d4d240e1f78a
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-21 16:24:00 -0500

    updated

---
+++ PostgreSQL_basic_SELECT_statement.sql
@@ -0,0 +1,3 @@
+-- Write a query to display the names (first_name, last_name) using an alias name “First Name", "Last Name".
+SELECT first_name "First Name",  last_name "Last Name" 
+FROM employees;
