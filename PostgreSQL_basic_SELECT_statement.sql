bfe5c91424013e8d152019259a1a0691632881bb
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-21 16:28:51 -0500

    updated

--- PostgreSQL_basic_SELECT_statement.sql
+++ PostgreSQL_basic_SELECT_statement.sql
@@ -1,3 +1,7 @@
 -- Write a query to display the names (first_name, last_name) using an alias name “First Name", "Last Name".
 SELECT first_name "First Name",  last_name "Last Name" 
 FROM employees;
+
+--  Write a query to get a unique department ID from employee table.
+SELECT DISTINCT department_id
+FROM employees;
