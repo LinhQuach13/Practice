47d35cd849e88b006237e430b25699b83bf8c71b
Linh Quach <Linh.M.Quach1@gmail.com>
2021-11-04 14:20:02 -0500

    updated

--- PostgreSQL_aggregate_function_and_groupBy.sql
+++ PostgreSQL_aggregate_function_and_groupBy.sql
@@ -3,3 +3,10 @@
     -- use count aggregation with distinct to get unique jobs only
 SELECT COUNT(DISTINCT job_id)
 FROM employees;
+
+-- 2.Write a query to get the total salaries payable to employees.
+   --- new calculation needed for total salaries
+   --- use salary column
+   --- aggregate on select statement
+SELECT SUM(salary) 
+FROM employees;
